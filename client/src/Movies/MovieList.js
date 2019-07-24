535c8217bcff32b448cf5ad51bd4ca64553da471
Red <vhanchar@ucsc.edu>
2019-07-23 17:39:21 -0700

    displays a list of movies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -21,7 +22,12 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+           <Link to={'/movies/${movie.id}'}>
+             <MovieDetails key={movie.id} movie={movie} />
+            
+             {/* <MovieDetails key={movie.id} movie={movie} /> */}
+           </Link>
+        // <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
@@ -32,7 +38,7 @@ function MovieDetails({ movie }) {
   return (
     <div className="movie-card">
       <h2>{title}</h2>
-      <div className="movie-director">
+      {/* <div className="movie-director">
         Director: <em>{director}</em>
       </div>
       <div className="movie-metascore">
@@ -44,7 +50,7 @@ function MovieDetails({ movie }) {
         <div key={star} className="movie-star">
           {star}
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
